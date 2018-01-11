71f6d0767803f6e0dc356bf24f37c4b7cc1a0878
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-01-11 10:07:07 +0530

    add blog app project for day 2

---
+++ apps/day-02/blog-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// const el = React.createElement(
+//   'h1',
+//   { style: { color: 'red' } },
+//   'Welcome to React'
+// );
+
+ReactDOM.render(<App />, document.getElementById('root'));
