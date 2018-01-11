7ec37f19411fcd1c52ec2dea8b779b1b2bcf65ba
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-01-11 23:40:00 +0530

    add apis for update and delete post operations

--- apps/day-04/blog-app/src/api/index.js
+++ apps/day-04/blog-app/src/api/index.js
@@ -15,3 +15,15 @@ export const addPost = post =>
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(post)
   }).then(response => response.json());
+
+export const updatePost = post =>
+  fetch(`${baseApiUrl}/posts/${post.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(post)
+  }).then(response => response.json());
+
+export const deletePost = id =>
+  fetch(`${baseApiUrl}/posts/${id}`, {
+    method: 'DELETE'
+  }).then(response => response.json());
